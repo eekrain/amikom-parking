f026fd3f3c468ae422d809c2d71cddeb95abcb93
Ardian Eka Candra <ardianoption@gmail.com>
2023-05-30 17:53:54 +0700

    configure using ssl and wss on dev

--- web/src/pages/QRCode.tsx
+++ web/src/pages/QRCode.tsx
@@ -1,40 +1,49 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createMemo, createSignal } from "solid-js";
 import dayjs from "dayjs";
 import { QRCodeSVG } from "solid-qr-code";
 import { createTimer } from "@solid-primitives/timer";
 
-const initDate = dayjs().add(30, "second");
+const QRKEY = import.meta.env.VITE_WEB_ADMIN_QRKEY;
+
+const dummyQRData = {
+  key: QRKEY,
+  validUntil: dayjs().add(1, "year"),
+};
 
 const Home: Component<{}> = (props) => {
-  const [qrData, setQrData] = createSignal(`${initDate.toISOString()}`);
   const [validUntil, setValidUntil] = createSignal(dayjs().add(30, "second"));
   const [validRemaining, setValidRemaining] = createSignal(
     validUntil().diff(dayjs(), "second")
   );
+  const qrData = createMemo(() => {
+    return {
+      key: QRKEY,
+      validUntil: validUntil(),
+    };
+  });
   const cb = () => {
     if (validRemaining() > 0) {
       const remainingSeconds = validUntil().diff(dayjs(), "second");
       setValidRemaining(remainingSeconds);
     } else {
       const newUntil = dayjs().add(30, "second");
-      setQrData(newUntil.toISOString());
       setValidUntil(newUntil);
       setValidRemaining(newUntil.diff(dayjs(), "second"));
     }
   };
   createTimer(cb, 1000, setInterval);
 
-  const [dummyQRData] = createSignal({
-    key: import.meta.env.VITE_WEB_ADMIN_QRKEY,
-    validUntil: dayjs().add(1, "year"),
-  });
-
   return (
     <>
       <h1 class="text-2xl text-center mb-6">Scan For Pass</h1>
       <div class="flex justify-center">
         <div class="bg-white rounded-lg shadow-md p-4">
-          <QRCodeSVG value={qrData()} width={350} height={350} class="mb-4" />
+          <QRCodeSVG
+            value={JSON.stringify(qrData())}
+            width={350}
+            height={350}
+            class="mb-4"
+          />
 
           <p class="text-2xl text-center">
             Valid for:{" "}
@@ -49,7 +58,7 @@ const Home: Component<{}> = (props) => {
           </p>
         </div>
       </div>
-      <p class="mt-24">dummy qr data: {JSON.stringify(dummyQRData())}</p>
+      <p class="mt-24">dummy qr data: {JSON.stringify(dummyQRData)}</p>
     </>
   );
 };
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    mkcert({
+      hosts: ["mydomain.com", "localhost"],
+    }),
+    solidPlugin(),
+  ],
   server: {
+    host: "mydomain.com",
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
